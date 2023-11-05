5fbbddc8a36fe2a22856a117caf72b6a3bc17f0b
Pedro Rafael Lázaro Nevado <119618710+Pedrop19@users.noreply.github.com>
2023-11-05 11:26:25 +0100

    Add script and include tags, update styles for footer and add new section for song list.

---
+++ script.js
@@ -0,0 +1,22 @@
+let listaElement = document.querySelector('.lista');
+
+fetch('./songs.json')
+.then(response => response.json())
+.then(data => {
+    const songs = data;
+    console.log(songs);
+
+    songs.forEach(song => {
+        let listElement = document.createElement("li");
+        listElement.className = "song";
+        listElement.innerHTML = `
+            <img src="${song.img}" width="100">
+            <div class="song-info">
+                <h2>${song.title}</h2>
+                <h3>${song.artist}</h3>
+                <p>${song.album}</p>
+            </div>
+        `;
+        listaElement.appendChild(listElement);
+    }); 
+})
