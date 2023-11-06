d861ee6ae6f7a402d2a5a01f7b89144d5fae9f7c
Pedro Rafael Lázaro Nevado <119618710+Pedrop19@users.noreply.github.com>
2023-11-06 19:03:50 +0100

    Update HTML, CSS, and JS files with new images and styles.

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ fetch('./songs.json')
     songs.forEach(song => {
         let listElement = document.createElement("li");
         listElement.className = "song";
+        listElement.style.background = `url(${song.img}) no-repeat top center / cover, linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))`;
         listElement.innerHTML = `
-            <img src="${song.img}" width="100">
             <div class="song-info">
                 <h2>${song.title}</h2>
                 <h3>${song.artist}</h3>
                 <p>${song.album}</p>
                 <p>${song.duration}</p>
+                <div class="song-play">
+                <i class="fa-regular fa-circle-play"></i>
+                </div>
             </div>
         `;
         listaElement.appendChild(listElement);
